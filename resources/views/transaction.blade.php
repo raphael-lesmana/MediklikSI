530b952ec80a2c7ca831240ef15dac379e0942a3
yenniferwilanata <yenniferwilanata@gmail.com>
2024-06-17 13:57:55 +0700

    trans

--- resources/views/transaction.blade.php
+++ resources/views/transaction.blade.php
@@ -1,21 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Unprocessed Transactions</h1>
-    @if (sizeof($transactions) > 0)
-        <ul>
-            @foreach ($transactions as $transaction)
-                <li>{{$transaction->id}} {{$transaction->patient->name}}</li>
-            @endforeach
-        </ul>
-    @else
-        No Unprocessed Transactions
-    @endif
-</body>
-</html>
+@extends('template.file')
+@extends('template.background')
+
+@section('title', 'Transactions')
+
+@section('content')
+<div>
+    <div class="d-flex justify-content-center mt-3 mb-5">
+        <h1>Unprocessed Transactions</h1>
+    </div>
+
+    <div>
+        @if (sizeof($transactions) > 0)
+            <ul>
+                @foreach ($transactions as $transaction)
+                    <li>{{$transaction->id}} {{$transaction->patient->name}}</li>
+                @endforeach
+            </ul>
+        @else
+            <div class="d-flex justify-content-center">
+            No Unprocessed Transactions
+            </div>
+        @endif
+    </div>
+</div>
+@endsection
