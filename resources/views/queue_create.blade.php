53e441b9620d18345ca33ed2257d60b238aca08a
yenniferwilanata <yenniferwilanata@gmail.com>
2024-06-17 11:30:30 +0700

    create queue

--- resources/views/queue_create.blade.php
+++ resources/views/queue_create.blade.php
@@ -1,22 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Add patient to queue</h1>
-    <form action={{route('queue.index')}} method="POST">
-        @csrf
-        <input type="text" name="patient_id" id="" placeholder="name">
-        <select name="staff_id" id="">
-            @foreach ($staff as $staff_member)
-                <option value={{$staff_member->id}}>{{$staff_member->full_name}}</option>
-            @endforeach
-        </select>
-        <button type="submit">Create</button>
-    </form>
-</body>
-</html>
+@extends('template.file')
+@extends('template.background')
+
+@section('title', 'Create Queue')
+
+@section('content')
+<div class="mt-5">
+    <div class="d-flex justify-content-center mb-3">
+        <h1>Add patient to queue</h1>
+    </div>    
+
+    <div class="d-flex justify-content-center">
+        <form action={{route('queue.index')}} method="POST">
+            @csrf
+            <input type="text" name="patient_id" id="" placeholder="name">
+            <select name="staff_id" id="">
+                @foreach ($staff as $staff_member)
+                    <option value={{$staff_member->id}}>{{$staff_member->full_name}}</option>
+                @endforeach
+            </select>
+
+            <div class="d-flex justify-content-center mt-2">
+                <button type="submit" class="btn btn-secondary">Create</button>
+            </div>
+        </form>
+    </div>
+
+</div>
+@endsection
