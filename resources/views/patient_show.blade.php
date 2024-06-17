cf86b0306568a37279c9bf9f60fd6c188ffabfd3
yenniferwilanata <yenniferwilanata@gmail.com>
2024-06-17 12:56:12 +0700

    patient

--- resources/views/patient_show.blade.php
+++ resources/views/patient_show.blade.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+@extends('template.file')
+@extends('template.background')
+
+@section("title", "Patient's Record")
+
+@section('content')
+
 <body>
     <tr>
         <td>{{$patient->name}}</td>
@@ -63,4 +62,5 @@
         </table>
     @endforeach
 </body>
-</html>
+
+@endsection
