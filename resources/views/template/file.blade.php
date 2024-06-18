6353f841742ad533ce95912ae6b6749d179b11d1
yenniferwilanata <yenniferwilanata@gmail.com>
2024-06-18 20:59:35 +0700

    navbar

--- resources/views/template/file.blade.php
+++ resources/views/template/file.blade.php
@@ -9,6 +9,28 @@
     </head>
 
     <body>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">MediKlik</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{route("queue")}}">Queue</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route("transaction")}}">Transaction</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route("patient")}}">Patient</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
         @yield('content')
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
